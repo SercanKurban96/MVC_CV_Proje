eda5d217214a747becbba3df5a12d2052e19d5bf
Sercan KURBAN <sercankurban1996@gmail.com>
2023-06-05 15:11:45 +0300

    Add project files.

---
+++ MVC_CV_Proje/Controllers/IletisimController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_CV_Proje.Models.Entity;
+using MVC_CV_Proje.Repositories;
+
+namespace MVC_CV_Proje.Controllers
+{
+    public class IletisimController : Controller
+    {
+        // GET: Iletisim
+        GenericRepository<TblIletisim> repo = new GenericRepository<TblIletisim>();
+        public ActionResult Index()
+        {
+            var mesajlar = repo.List();
+            return View(mesajlar);
+        }
+    }
+}
---
+++ MVC_CV_Proje/Repositories/DeneyimRepository.cs
@@ -0,0 +1,13 @@
+﻿using MVC_CV_Proje.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_CV_Proje.Repositories
+{
+    public class DeneyimRepository : GenericRepository<TblDeneyimlerim>
+    {
+        
+    }
+}
